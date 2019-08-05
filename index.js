404e6d5a63ea95fe84079932854e163aba0911fb
Zolton <gmzesq@gmail.com>
2019-08-05 15:22:09 -0400

    Initial push/pull

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+
+const express = require("express");
+const server = express();
+const port = 8000;
+server.use(express.json())
+const users = require("./data/db")
+
+server.get("/", (req, res)=>{
+    res.send("GET working")
+})
+
+server.get("")
+
+server.listen(port, ()=>{console.log("API running")})
